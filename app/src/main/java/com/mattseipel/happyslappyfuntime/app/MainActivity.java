545d2119ab30d2295aff5b21c8a6bc54a200c33d
Matt Seipel <seipel_5@hotmail.com>
2014-03-09 20:08:13 -0500

    Testing.  Check 1, check 2

--- app/src/main/java/com/mattseipel/happyslappyfuntime/app/MainActivity.java
+++ app/src/main/java/com/mattseipel/happyslappyfuntime/app/MainActivity.java
@@ -8,6 +8,8 @@ import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
+    //GIT TEST MOTHERFUCKER
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
